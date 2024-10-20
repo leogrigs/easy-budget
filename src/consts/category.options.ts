97c5592e6bb003d968a1dced99354dc854497489
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-10-20 07:52:38 -0300

    chore(options): create consts for selects

---
+++ src/consts/category.options.ts
@@ -0,0 +1,34 @@
+import { InputOptions } from "../interfaces/InputOptions.interface";
+
+export const CATEGORY_OPTIONS: InputOptions[] = [
+  {
+    id: "category-1",
+    label: "salary",
+    value: "salary",
+  },
+  {
+    id: "category-2",
+    label: "Food",
+    value: "food",
+  },
+  {
+    id: "category-3",
+    label: "Transport",
+    value: "transport",
+  },
+  {
+    id: "category-4",
+    label: "Miscellaneous",
+    value: "miscellaneous",
+  },
+  {
+    id: "category-5",
+    label: "Entertainment",
+    value: "entertainment",
+  },
+  {
+    id: "category-6",
+    label: "Other",
+    value: "other",
+  },
+];
