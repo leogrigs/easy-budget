0e118f6cf9d5202a72ba38451928d367d5108771
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-10-21 08:51:39 -0300

    style(totalizers): initial style structure

--- src/components/Totalizers/Totalizers.tsx
+++ src/components/Totalizers/Totalizers.tsx
@@ -5,10 +5,27 @@ type TotalizersProps = {
 
 const Totalizers: React.FC<TotalizersProps> = ({ income, expense }) => {
   return (
-    <div>
-      <div>Balance: {income - expense}</div>
-      <div>Income: {income}</div>
-      <div>Expenses: {expense}</div>
+    <div className="flex gap-4">
+      <div className="flex flex-col">
+        <span>Balance</span>
+        <span>
+          <span>BRL</span> {income - expense}
+        </span>
+      </div>
+
+      <div className="flex flex-col">
+        <span>Income</span>
+        <span>
+          <span>BRL</span> {income}
+        </span>
+      </div>
+
+      <div className="flex flex-col">
+        <span>Expense</span>
+        <span>
+          <span>BRL</span> {expense}
+        </span>
+      </div>
     </div>
   );
 };
