699b390cfea4341f901fdd7bc9c2b30e969cc0d1
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-10-24 08:17:20 -0300

    fix: svg properties

--- src/components/BudgetTable/BudgetTable.tsx
+++ src/components/BudgetTable/BudgetTable.tsx
@@ -59,9 +59,9 @@ const BudgetTable: React.FC<BudgetTableProps> = ({ rows, itemsPerPage }) => {
           className="absolute w-8 h-8 top-2 right-0 text-slate-400"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
         </svg>
       </div>
