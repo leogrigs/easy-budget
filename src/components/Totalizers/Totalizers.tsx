8cfa54505e2720b854b6d13fcb398b140cd5647e
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-11-02 10:34:59 -0300

    feat(theme): dark mode totalizers component

--- src/components/Totalizers/Totalizers.tsx
+++ src/components/Totalizers/Totalizers.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type TotalizersProps = {
   income: number;
   expense: number;
@@ -8,33 +10,34 @@ const Totalizers: React.FC<TotalizersProps> = ({ income, expense }) => {
     {
       label: "Balance",
       value: income - expense,
-      color: "#475569",
+      colorClass: "text-slate-800 dark:text-slate-400",
     },
     {
       label: "Income",
       value: income,
-      color: "#166534",
+      colorClass: "text-green-800 dark:text-green-400",
     },
     {
       label: "Expense",
       value: expense,
-      color: "#991b1b",
+      colorClass: "text-red-800 dark:text-red-400",
     },
   ];
 
-  const formatNumber = (value: number) => {
-    return new Intl.NumberFormat("pt-BR", {
+  const formatNumber = (value: number) =>
+    new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL",
     }).format(value);
-  };
 
   return (
-    <div className="flex gap-8">
+    <div className="flex gap-8 justify-around py-4">
       {data.map((item) => (
         <div className="flex flex-col" key={item.label}>
-          <span className="text-base text-slate-800">{item.label}</span>
-          <span className="text-2xl font-light" style={{ color: item.color }}>
+          <span className="text-base font-light text-slate-600 dark:text-slate-300">
+            {item.label}
+          </span>
+          <span className={`text-2xl ${item.colorClass}`}>
             {formatNumber(item.value)}
           </span>
         </div>
