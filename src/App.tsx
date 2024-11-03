6f9ae150c76bb6ce4777dfe9d881e9581091e3e6
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-11-03 10:18:20 -0300

    feat(responsivity): app component

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,18 @@ function App() {
     <>
       <div className="min-h-screen flex flex-col bg-gradient-to-b from-white to-slate-50 dark:from-slate-900 dark:to-gray-800">
         {/* Header */}
-        <header className="flex justify-between items-center border-b border-slate-200 dark:border-slate-700 py-4 px-8 shadow-md">
+        <header className="flex flex-col md:flex-row justify-between items-center border-b border-slate-200 dark:border-slate-700 py-4 px-4 md:px-8 shadow-md">
           <div className="flex items-center space-x-2">
-            <img src={logo} alt="Easy Budget Logo" className="h-12" />
-            <h1 className="text-2xl font-bold text-slate-800 dark:text-slate-100">
+            <img src={logo} alt="Easy Budget Logo" className="h-10 md:h-12" />
+            <h1 className="text-xl md:text-2xl font-bold text-slate-800 dark:text-slate-100">
               Easy Budget
             </h1>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 mt-2 md:mt-0">
             <ThemeToggle />
             {user && (
               <button
-                className="px-4 py-2 text-sm font-medium text-white bg-teal-600 dark:bg-teal-500 rounded-md hover:bg-teal-500 dark:hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-400 dark:focus:ring-offset-gray-800"
+                className="px-3 md:px-4 py-1 md:py-2 text-xs md:text-sm font-medium text-white bg-teal-600 dark:bg-teal-500 rounded-md hover:bg-teal-500 dark:hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-400 dark:focus:ring-offset-gray-800"
                 onClick={logout}
               >
                 Logout
@@ -45,7 +45,7 @@ function App() {
         </header>
 
         {/* Main Content */}
-        <div className="flex-grow flex px-8">
+        <div className="flex-grow flex px-4 py-6 md:px-8 md:py-12 justify-center">
           {/* Check if user is authenticated */}
           {!user ? <Auth onUserLogin={setUser} /> : <System user={user} />}
         </div>
